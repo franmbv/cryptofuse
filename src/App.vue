5318db4b04bcf3dea622379c0d6796958c657536
CodeSandbox <support@codesandbox.io>
2023-12-07 21:07:41 +0000

    Sandbox snapshot commit

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@ import NavBar from "./components/NavBar.vue";
 import SwapForm from "./components/SwapForm.vue";
 import Footer from "./components/Footer.vue";
 import Carousel from "./components/Carousel.vue";
-import CryptoFile from "./assets/crypto_file.json";
 
 </script>
 
